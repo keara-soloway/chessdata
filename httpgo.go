3272ea3c324fa38888e89756d223d650c045177f
Valentin Kuznetsov <vkuznet@gmail.com>
2022-09-22 08:14:09 -0400

    Add payload API

--- httpgo.go
+++ httpgo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -11,6 +12,7 @@ import (
 	"net/http/httputil"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -27,6 +29,111 @@ var Config Configuration
 
 // version represents version of the server
 var version string
+
+// Record represent generic record
+type Record map[string]interface{}
+
+// helper function to generate series of records for given number of rows
+func genNRecords(total int) []Record {
+	var records []Record
+	slice := make([]byte, 1024)
+	size := runtime.Stack(slice, false)
+	for i := 0; i < total; i++ {
+		rec := make(Record)
+		rec["id"] = i
+		rec["data"] = slice[0:size]
+		records = append(records, rec)
+	}
+	return records
+}
+
+// helper function to generate series of records totaling in size to given value
+func genRecords(size string) ([]Record, error) {
+	var records []Record
+	var suffix string
+	if strings.HasSuffix(size, "KB") {
+		suffix = "KB"
+	} else if strings.HasSuffix(size, "MB") {
+		suffix = "MB"
+	} else if strings.HasSuffix(size, "GB") {
+		suffix = "GB"
+	} else {
+		return nil, errors.New("unsupported size, should be KB, MB or GB units")
+	}
+	arr := strings.Split(size, suffix)
+	total, err := strconv.Atoi(arr[0])
+	if err != nil {
+		return nil, err
+	}
+	records = genNRecords(total)
+	return records, nil
+}
+
+// HTTPError function dumpt http error to log and return back to user
+func HTTPError(label, msg string, w http.ResponseWriter) {
+	log.Println(label, msg)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte(msg))
+}
+
+// PayloadHandler provides API to test the payload
+func PayloadHandler(w http.ResponseWriter, r *http.Request) {
+	var latency int
+	var size string
+	var format string
+	for k, values := range r.URL.Query() {
+		if k == "latency" {
+			v, err := strconv.Atoi(values[0])
+			if err == nil {
+				latency = v
+			} else {
+				msg := fmt.Sprintf("unable to convert latency value, error %v", err)
+				HTTPError("ERROR", msg, w)
+				return
+			}
+		} else if k == "size" {
+			size = values[0]
+		} else if k == "format" {
+			format = values[0]
+		}
+	}
+	if latency > 0 {
+		time.Sleep(time.Duration(latency) * time.Second)
+	}
+	if format != "json" && format != "ndjson" {
+		msg := fmt.Sprintf("unsupported format %s", format)
+		HTTPError("ERROR", msg, w)
+		return
+	}
+
+	records, err := genRecords(size)
+	if err != nil {
+		msg := fmt.Sprintf("unable to generate records, error %v", err)
+		HTTPError("ERROR", msg, w)
+		return
+	}
+	if format == "json" {
+		data, err := json.Marshal(records)
+		if err == nil {
+			w.Write(data)
+			return
+		}
+		msg := fmt.Sprintf("unable to marshal records, error %v", err)
+		HTTPError("ERROR", msg, w)
+		return
+	} else if format == "ndjson" {
+		for _, rec := range records {
+			data, err := json.Marshal(rec)
+			if err != nil {
+				msg := fmt.Sprintf("unable to marshal records, error %v", err)
+				HTTPError("ERROR", msg, w)
+				return
+			}
+			w.Write(data)
+			w.Write([]byte("\n"))
+		}
+	}
+}
 
 // RequestHandler handles incoming HTTP request
 func RequestHandler(w http.ResponseWriter, r *http.Request) {
@@ -101,6 +208,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	http.HandleFunc("/payload", PayloadHandler)
 	http.HandleFunc("/", RequestHandler)
 	if Config.ServerKey != "" && Config.ServerCrt != "" {
 		server := &http.Server{
